c0821c968b9a64c54ff9ec5fe8a9eec8f9640092
Leila <leila.dardouri@outlook.it>
2021-02-01 12:41:40 +0100

    infoorganismo

--- QTAquarius/src/organismoinfoview.cpp
+++ QTAquarius/src/organismoinfoview.cpp
@@ -17,16 +17,12 @@ OrganismoInfoView::OrganismoInfoView(QWidget* parent) : QDialog(parent) {
     setStyleSheet(styleSheet);
 
     setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);  //disable "?" button
-    setMinimumSize(QSize(280, 180));
-    setMaximumSize(QSize(280, 180));
+    setMinimumSize(QSize(280, 210));
+    setMaximumSize(QSize(280, 210));
     move(parent->geometry().center());
 
     layout = new QGridLayout(this);
-    /*
-    // IMAGE
-    img = new QLabel(this);
-    layout->addWidget(img, 0, 0, 1, 1);
-    */
+
     // NAME
     nameLine = new QLineEdit(this);
     nameLine->setMaxLength(60);
@@ -40,16 +36,26 @@ OrganismoInfoView::OrganismoInfoView(QWidget* parent) : QDialog(parent) {
     layout->addWidget(new QLabel("Tipologia:", this), 1, 0, 1, 1);
     layout->addWidget(tipologia, 1, 1, 1, 1);
 
-    // ValoreNutrizionale
+    // NUTRITIONAL VALUE
     nutVal = new QLabel("", this);
     layout->addWidget(new QLabel("Valore nutrizionale:", this), 2, 0, 1, 1);
     layout->addWidget(nutVal, 2, 1, 1, 1);
 
+    // HUNGRY OR NOT
+    hungry = new QLabel("", this);
+    layout->addWidget(new QLabel("Affamato:", this), 3, 0, 1, 1);
+    layout->addWidget(hungry, 3, 1, 1, 1);
+
+    // STATUS
+    status = new QLabel("", this);
+    layout->addWidget(new QLabel("Stato:", this), 4, 0, 1, 1);
+    layout->addWidget(status, 4, 1, 1, 1);
+
     // STAMINA
-    bar = new QProgressBar(this);
-    bar->setMaximum(100);
-    layout->addWidget(new QLabel("Stamina:", this), 4, 0, 1, 1);
-    layout->addWidget(bar, 4, 1, 1, 2);
+    staminaBar = new QProgressBar(this);
+    staminaBar->setMaximum(100);
+    layout->addWidget(new QLabel("Stamina:", this), 5, 0, 1, 1);
+    layout->addWidget(staminaBar, 5, 1, 1, 2);
 
     // NEXT PREV
     previous = new QPushButton("Precedente", this);
@@ -57,13 +63,14 @@ OrganismoInfoView::OrganismoInfoView(QWidget* parent) : QDialog(parent) {
     next = new QPushButton("Successivo", this);
     connect(previous, &QPushButton::released, this, [this]() { controller->prev(); updateInfo(); });
     connect(next, &QPushButton::released, this, [this]() { controller->next(); updateInfo(); });
-    layout->addWidget(previous, 5, 0, 1, 1);
-    layout->addWidget(currentmax, 5, 1, 1, 1, Qt::AlignCenter);
-    layout->addWidget(next, 5, 2, 1, 1);
+    layout->addWidget(previous, 6, 0, 1, 1);
+    layout->addWidget(currentmax, 6, 1, 1, 1, Qt::AlignCenter);
+    layout->addWidget(next, 6, 2, 1, 1);
 
+    // QUIT
     quitButton = new QPushButton("Chiudi", this);
     connect(quitButton, &QPushButton::released, this, [this]() { killTimer(timerID); QDialog::close(); });
-    layout->addWidget(quitButton, 6, 2, 1, 1);
+    layout->addWidget(quitButton, 7, 2, 1, 1);
 
     setLayout(layout);
 }
@@ -79,24 +86,29 @@ void OrganismoInfoView::updateInfo() {
         setWindowTitle("Info Oganismo");
         nameLine->setText("");
         nameLine->setReadOnly(true);
+        tipologia->setText("");
         nutVal->setNum(0);
-        bar->setValue(0);
+        hungry->setText("");
+        status->setText("");
+        staminaBar->setValue(0);
         currentmax->setText("0 di 0");
         previous->setDisabled(true);
         next->setDisabled(true);
-        return;
     } else {
         setWindowTitle(("Info oganismo: "+ o->getName()).c_str());
-        /*
-        QPixmap pix = QPixmap(":/images/punto.png");
-        pix = pix.scaled(img->size(), Qt::KeepAspectRatio);
-        img->setPixmap(pix);
-        */
         nameLine->setText(o->getName().c_str());
         nameLine->setReadOnly(false);
         tipologia->setText(QString::fromStdString(o->getSpecie()));
         nutVal->setNum(o->getValoreNutrizionale());
-        bar->setValue(o->getStamina()*100);
+        if(o->isHungry())
+            hungry->setText(QString("Sì"));
+        else
+            hungry->setText(QString("No"));
+        if(o->isAwake())
+            status->setText(QString("Sveglio"));
+        else
+            status->setText(QString("Addormentato"));
+        staminaBar->setValue(o->getStamina()*100);
         std::stringstream s;
         s << (controller->getPosition() + 1) << " di " << controller->getVectorSize();
         currentmax->setText(s.str().c_str());
@@ -116,6 +128,6 @@ void OrganismoInfoView::setController(Controller* c) {
     controller = c;
 }
 
-void OrganismoInfoView::timerEvent(QTimerEvent*) {  //aggiorna la progress bar con la stamina
+void OrganismoInfoView::timerEvent(QTimerEvent*) {  //per aggiornare progress bar
     updateInfo();
 }
